22ba51428695e6ac69c5ee946a52943f9c0508a6
Ugudei <ugudeit@gmail.com>
2019-12-16 12:36:54 -0700

    Update HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,24 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age,color,food,job,honeyPot) {
+  Bee.call(this,age,color,food,job);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
+
+
+
+
+
+
 };
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  console.log(this.honeyPot);
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot--
+}
 
