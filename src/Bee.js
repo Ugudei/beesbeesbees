6fb159a7d346374be2532f8946ced2dc70df2aca
Ugudei <ugudeit@gmail.com>
2019-12-16 12:32:49 -0700

    Update Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-var Bee = function() {
+var Bee = function(age,color,food,job) {
+Grub.call(this,age,color,food);
+this.job = 'keep on growing';
+this.age = 5;
+this.color = 'yellow';
+
+
+
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
