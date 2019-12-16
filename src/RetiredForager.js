94742fccb42200185df845772cc5eb42e75cdd10
Ugudei <ugudeit@gmail.com>
2019-12-16 12:37:14 -0700

    RetiredBee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,22 @@
-var RetiredForagerBee = function() {
-};
+var RetiredForagerBee = function(age,color,food,job,canFly,treasureChest) {
+  ForagerBee.call(this,age,color,job,canFly,treasureChest);
+  Grub.call(food);
 
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
+
+};
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function(){
+
+  return 'I am too old, let me play cards instead';
+}
+RetiredForagerBee.prototype.gamble = function(treasure){
+
+  this.treasureChest.push(treasure);
+}
